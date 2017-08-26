d7b5435ca9202d17921ee07179e9f32689df7b57
Guilherme Borba <Brobz@Guilhermes-MacBook-Pro.local>
2017-08-26 18:28:08 -0500

    update

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get("/login", function(req, res){
     res.sendFile(__dirname + "/client/login.html");
 });
 
+app.get("/signup", function(req, res){
+    res.sendFile(__dirname + "/client/signup.html");
+});
+
 app.use("/client", express.static(__dirname + "/client"));
 
 
